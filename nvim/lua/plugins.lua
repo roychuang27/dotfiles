450362eca8ca920ff93b89e0de1b2bfc7499592c
roychuang <roychuang27@protonmail.com>
2026-04-03 22:23:22 +0800

    .

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -23,10 +23,36 @@ return {
         end,
     },
     {
-        "vim-airline/vim-airline-themes",
-        config = function()
-            vim.g.airline_theme = "base16_adwaita"
-        end,
+      "nvim-lualine/lualine.nvim",
+      dependencies = {
+        "nvim-tree/nvim-web-devicons",
+      },
+      config = function()
+        require("lualine").setup({
+          options = {
+            theme = "auto",
+            globalstatus = true,
+            component_separators = { left = "", right = "" },
+            section_separators = { left = "", right = "" },
+          },
+          sections = {
+            lualine_a = { "mode" },
+            lualine_b = { "branch", "diff", "diagnostics" },
+            lualine_c = { { "filename", path = 1 } },
+            lualine_x = { "encoding", "fileformat", "filetype" },
+            lualine_y = { "progress" },
+            lualine_z = { "location" },
+          },
+          inactive_sections = {
+            lualine_a = {},
+            lualine_b = {},
+            lualine_c = { { "filename", path = 1 } },
+            lualine_x = { "location" },
+            lualine_y = {},
+            lualine_z = {},
+          },
+        })
+      end,
     },
     {
         "lukas-reineke/indent-blankline.nvim",
